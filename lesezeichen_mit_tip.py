ea6bd491e2f789c2ba878cf9c4c9a6cd6b838ff0
danajasmine <danaengel73@gmail.com>
2017-01-12 11:02:09 +0100

    Lesezeichen.py

---
+++ lesezeichen_mit_tip.py
@@ -0,0 +1,17 @@
+x = 0
+
+# Funktion, die 3 Ausgaben macht
+def function():
+	global x
+	x = x + 1
+	
+	if (x == 1):
+		print("Hallo")
+	elif (x == 2):
+                print("hey")
+                
+        else:                print("tschüss")
+        
+function()
+function()
+function()
